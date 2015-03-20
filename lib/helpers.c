b6401209457c1b5b921a05508a29300b84800e8a
itegulov <wibkwibk@gmail.com>
2015-03-20 18:27:46 +0300

    Reading more data

--- lib/helpers.c
+++ lib/helpers.c
@@ -45,15 +45,17 @@ ssize_t read_until_word(int fd, void *buf, size_t count, char *word, size_t word
 	ssize_t matched = 0;
 	char *chars = (char *) buf;
 	for (size_t offset = 0;;) {
-		cnt = read_(fd, buf + offset, 1);
+		cnt = read_(fd, buf + offset, word_length - matched);
 		if (cnt == -1)
 				return -1;
 		if (cnt == 0)
 			return offset;
-		if (chars[offset++] == word[matched]) {
-			matched++;
-		} else {
-			matched = 0;
+		for (int i = 0; i < cnt; i++) {
+			if (chars[offset++] == word[matched]) {
+				matched++;
+			} else {
+				matched = 0;
+			}
 		}
 		if (matched == word_length) {
 			return offset;
