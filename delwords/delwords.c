7a2601f657d880cf9b25820738609def8eb47b45
itegulov <wibkwibk@gmail.com>
2015-03-20 18:18:46 +0300

    Done with symbol-by-symbol reading

--- delwords/delwords.c
+++ delwords/delwords.c
@@ -1,7 +1,40 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "helpers.h"
+#include "string.h"
 
-int main() {
-	
+#define BUF_SIZE 2048
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("Usage: ./delwords <word>\n");
+		return EXIT_FAILURE;
+	}
+	char buf[BUF_SIZE];
+	char *word = argv[1];
+
+	ssize_t length = strlen(word);
+	while(1) {
+		ssize_t cnt = read_until_word(STDIN_FILENO, buf, BUF_SIZE, word, length);
+		//printf("GOT: %s", buf);
+		if (cnt == -1)
+			return EXIT_FAILURE;
+		if (cnt == 0)
+			return EXIT_SUCCESS;
+		if (cnt >= length) {
+			int matched = 1;
+			for (ssize_t i = 0; i < length; i++) {
+				//printf("%c =?= %c\n", buf[cnt - length + i], word[i]);
+				if (buf[cnt - length + i] != word[i]) {
+					matched = 0;
+					break;
+				}
+			}
+			if (matched) {
+				cnt -= length;
+			}
+		}
+		if (write_(STDOUT_FILENO, buf, cnt) == -1)
+			return EXIT_FAILURE;
+	}
 }
--- lib/helpers.c
+++ lib/helpers.c
@@ -18,7 +18,7 @@ ssize_t write_(int fd, const void *buf, size_t count) {
     size_t offset = 0;
     do {
         cnt = write(fd, buf + offset, count);
-        if (cnt == -1) 
+        if (cnt == -1)
             return -1;
         count -= cnt;
         offset += cnt;
@@ -30,7 +30,7 @@ ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
     ssize_t cnt = 0;
     char *chars = (char *) buf;
     for (size_t offset = 0;;) {
-        cnt = read(fd, buf + offset, 1);
+        cnt = read_(fd, buf + offset, 1);
         if (cnt == -1)
             return -1;
         if (cnt == 0)
@@ -38,4 +38,25 @@ ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
         if (chars[offset++] == delimiter || offset == count)
             return offset;
     }
+}
+
+ssize_t read_until_word(int fd, void *buf, size_t count, char *word, size_t word_length) {
+	size_t cnt = 0;
+	ssize_t matched = 0;
+	char *chars = (char *) buf;
+	for (size_t offset = 0;;) {
+		cnt = read_(fd, buf + offset, 1);
+		if (cnt == -1)
+				return -1;
+		if (cnt == 0)
+			return offset;
+		if (chars[offset++] == word[matched]) {
+			matched++;
+		} else {
+			matched = 0;
+		}
+		if (matched == word_length) {
+			return offset;
+		}
+	}
 }
