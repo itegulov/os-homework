ad43d81d8ddd679c6238db38490f298f735caac9
itegulov <wibkwibk@gmail.com>
2015-05-29 02:17:09 +0300

    Implemented simplesh

--- simplesh/simplesh.c
+++ simplesh/simplesh.c
@@ -1,36 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <helpers.h>
+#include <bufio.h>
+#include <signal.h>
+#include <errno.h>
 
 #define BUF_SIZE 4096
 
+void print_line(int sig) {
+	write_(STDOUT_FILENO, "\n", 1);
+}
+
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		puts("Usage: filter <executable file>");
-		return -1;
-	}
+	char buf[BUF_SIZE];
 
-	char buf[BUF_SIZE + 1];
+	buf_t *buffota = buf_new(BUF_SIZE);
+	signal(SIGINT, print_line);
 
-	char *args[argc + 1];
-	for (int i = 0; i < argc - 1; i++) {
-		args[i] = argv[i + 1];
-	}
-	args[argc - 1] = buf;
-	args[argc] = NULL;
-	ssize_t n;
-	while ((n = read_until(STDIN_FILENO, buf, BUF_SIZE, '\n')) != 0) {
-		if (n == -1) {
-			perror("couldn't read");
+	for (;;) {
+		write_(STDOUT_FILENO, "$", 1);
+		ssize_t res = buf_getline(STDIN_FILENO, buffota, '\n', buf);
+		printf("%d\n", res);
+		if (res < 0) {
+			if (errno == EINTR) {
+				continue;
+			}
 			return EXIT_FAILURE;
-		} else {
-			if (buf[n - 1] == '\n')
-				buf[n - 1] = '\0';
-			else
-				buf[n] = '\0';
-			if (spawn(argv[1], args) == 0)
-				puts(buf);
+		}
+		if (res == 0) {
+			return 0;
+		}
+		buf[res] = 0;
+		int programs = 1;
+		for (ssize_t i = 0; i < res; i++) {
+			if (buf[i] == '|') {
+				programs++;
+				buf[i] = 0;
+			}
+		}
+
+		printf("%d\n", programs);
+
+		execargs_t e[programs];
+		execargs_t* ep[programs];
+		ssize_t last_pos = 0;
+		int cnt = 0;
+		for (ssize_t i = 0; i <= res; i++) {
+			printf("DEBUG: %c\n", buf[i]);
+			if (buf[i] == 0) {
+				int args = (buf[i - 1] != ' ');
+				ssize_t last_space = last_pos - 1;
+				for (ssize_t j = last_pos; j < i; j++) {
+					if (buf[j] == ' ') {
+						if (last_space != j - 1) {
+							args++;
+						}
+						buf[j] = 0;
+						last_space = j;
+					}
+				}
+				char* proc_args[args + 1];
+				proc_args[args] = 0;
+				ssize_t last_arg = last_pos;
+				int arg_cnt = 0;
+				for (ssize_t j = last_pos; j <= i; j++) {
+					if (buf[j] == 0) {
+						if (last_arg != j) {
+							proc_args[arg_cnt++] = buf + last_arg;
+						}
+						last_arg = j + 1;
+					}
+				}
+				printf("KUDAH KUKAREK: %d %s\n", args, proc_args[0]);
+
+				if (!(e[cnt++] = new_execargs(proc_args))) {
+					for (int j = 0; j < cnt - 1; j++) {
+						free_execargs(e[j]);
+					}
+					write_(STDOUT_FILENO, "Out of memory\n", 14);
+					break;
+				} else {
+					ep[cnt - 1] = e + cnt - 1;
+				}
+				last_pos = i + 1;
+			}
+		}
+
+		printf("RUNNING\n");
+		runpiped(ep, programs);
+		printf("RUNNED\n");
+
+		for (int i = 0; i < programs; i++) {
+			free_execargs(e[i]);
 		}
 	}
-	return EXIT_SUCCESS;
 }
